2945234bb5c403b084342097be2f0cd4eb19d4da
anisargsian <annie.sargsyan@gmail.com>
2018-06-07 19:34:12 +0400

    added proptypes to MemberGitHubAccount component

--- src/components/MemberGitHubAccount/MemberGitHubAccount.jsx
+++ src/components/MemberGitHubAccount/MemberGitHubAccount.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const MemberGitHubAccount = ({href, className}) => {
     return (
         <a className={className} href={href}>GitHub Account</a>
     );
+};
+
+MemberGitHubAccount.propTypes = {
+    className: PropTypes.string,
+    href: PropTypes.string.isRequired,
 };
 
 export default MemberGitHubAccount;
