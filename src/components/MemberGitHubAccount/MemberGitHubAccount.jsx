aacc09f0c6d3fe0869b3908171a0c110a1eaf7c9
anisargsian <annie.sargsyan@gmail.com>
2018-06-07 19:16:27 +0400

    Added MemberGitHubAccount component

---
+++ src/components/MemberGitHubAccount/MemberGitHubAccount.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const MemberGitHubAccount = ({href, className}) => {
+    return (
+        <a className={className} href={href}>GitHub Account</a>
+    );
+};
+
+export default MemberGitHubAccount;
