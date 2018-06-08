415ee6b656ba53a8e8711bd71d30a8280fe3149d
Tatev Grigoryan <tatev@codics.am>
2018-06-08 14:31:24 +0400

    1. improved components structure

--- src/data/headerData.js
+++
@@ -1,9 +0,0 @@
-const headerData = {
-    menuItems: [
-        { value: "concepts", href: "#" },
-        { value: "our team", href: "#" },
-    ],
-    gitHubLink: "https://github.com/javascript-under-the-hood/javascript-under-the-hood/",
-};
-
-export default headerData;
