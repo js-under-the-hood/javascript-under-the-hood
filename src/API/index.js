a799b7df4e8e5f3d618f56038bab2e01d9b6628a
javascript-under-the-hood <40025159+javascript-under-the-hood@users.noreply.github.com>
2018-06-11 15:17:20 +0400

    Update index.js

--- src/API/index.js
+++ src/API/index.js
@@ -1,6 +1,6 @@
 const gitHubUserFetch = (username) => {
     return fetch(`https://api.github.com/users/${username}`, {
-        headers: {'Authorization': 'token 23692ff3437d10f4124b90bed48e695efa197157'}
+        headers: {'Authorization': 'token 24ece0ab7d626b6c14561eec2e059a1b29b872ce'}
     })
 };
 
